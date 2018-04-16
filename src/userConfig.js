88daf2be577c5be48e640f53904501bf8057c760
BroccoliSpring <Lensgcx@163.com>
2018-04-16 15:59:28 +0800

    user config
    user config

--- src/userConfig.js
+++ src/userConfig.js
@@ -11,27 +11,24 @@ import './assets/css/public.scss'//public css
 //ele cover css
 import './assets/css/element-ui/theme-cover/base.scss'
 import './assets/css/element-ui/theme-cover/data.scss'
-
 import './assets/css/element-ui/theme-cover/navigation.scss'
 import './assets/css/element-ui/theme-cover/notice.scss'
 import './assets/css/element-ui/theme-cover/other.scss'
-
 import './assets/css/element-ui/theme-cover/form.scss'
 
 
 /* ---------------- js ( Framework or auxiliary function ) ---------------- */
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import vue_formercool_Cx from './lib/index.js';//cx formercool
+import cxEasyForm from '../dist/cx-easy-form.min';//cx formercool
+import '../dist/cx-easy-form.min.css';
+
 import formEvent from './utils/formEvent'
-// import vue_auto_form_test from 'vue-auto-form-test';
-// Vue.use(vue_auto_form_test, {stageName: 'gcx-'})
 
 Vue.use(ElementUI);
-Vue.use(vue_formercool_Cx.body, {
+Vue.use(cxEasyForm.body, {
   stageName: 'cx',//stage name
   log: false,//show log information
-  store: {},
   formEvent: formEvent,//binding form component interaction method （click / change / blur / fouce ... and so on）
   keyRefer: {
     // "auto-form": 12312,
