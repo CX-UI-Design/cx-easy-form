5d91205522d54667dbb496ed11951b5b8e94610d
BroccoliSpring <Lensgcx@163.com>
2018-04-24 23:35:50 +0800

    role button index
    role button index

---
+++ src/lib/packages/Button/Role-button/index.js
@@ -0,0 +1,9 @@
+import GcxRoleButton from './src/role-button.vue';
+/* istanbul ignore next */
+GcxRoleButton.install = function (Vue) {
+  Vue.component(GcxRoleButton.name, GcxRoleButton);
+};
+
+export default GcxRoleButton;
+
+
