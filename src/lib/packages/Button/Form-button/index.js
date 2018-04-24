3c53a486fa01bb9c2208c3857669553105fbc5c7
BroccoliSpring <Lensgcx@163.com>
2018-04-24 23:36:02 +0800

    form button index
    form button index

---
+++ src/lib/packages/Button/Form-button/index.js
@@ -0,0 +1,9 @@
+import GcxFormButton from './src/form-button.vue';
+/* istanbul ignore next */
+GcxFormButton.install = function (Vue) {
+  Vue.component(GcxFormButton.name, GcxFormButton);
+};
+
+export default GcxFormButton;
+
+
