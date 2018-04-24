9d55d4e3f643d3ddc18c650b2f1b892e0cea5515
BroccoliSpring <Lensgcx@163.com>
2018-04-24 23:36:25 +0800

    auto form operation
    auto form operation

--- src/lib/packages/Auto-form-operation/src/auto-form-operation.vue
+++ src/lib/packages/Auto-form-operation/src/auto-form-operation.vue
@@ -1,10 +1,28 @@
 <!--auto-form-operation 自动表单-操作模块部分-->
 <template>
   <div class="cs-auto-form-operation">
-    <component :is="$SN+'button'" v-for="(btnItem,index) in buttonInfo" :index="index" :key="index" :type="btnItem.style"
-               :size="btnItem.size" :plain="btnItem.plain" :round="btnItem.round" :disabled="btnItem.disabled"
-               :roleInfo="btnRoleInfo(btnItem)" @click="btnItem.event(vm,autoFormID)">
-    </component>
+    <div v-if="type === 'role'">
+      <component :is="$SN+'role-button'" v-for="(btnItem,index) in buttonInfo" :index="index" :key="index" :type="btnItem.style"
+                 :name="btnItem.name" :size="btnItem.size" :plain="btnItem.plain" :round="btnItem.round" :disabled="btnItem.disabled"
+                 :roleInfo="btnRoleInfo(btnItem)" :roleButtonList="roleButtonList" @click="btnItem.event(vm,autoFormID)">
+        {{btnItem.name}}
+      </component>
+    </div>
+    <div v-else-if="type === 'form'">
+      <component :is="$SN+'form-button'" v-for="(btnItem,index) in buttonInfo" :index="index" :key="index" :type="btnItem.style"
+                 :size="btnItem.size" :plain="btnItem.plain" :round="btnItem.round" :disabled="btnItem.disabled"
+                 @click="btnItem.event(vm,autoFormID)">
+        {{btnItem.name}}
+      </component>
+    </div>
+    <div v-else>
+      <component :is="$SN+'button'" v-for="(btnItem,index) in buttonInfo" :index="index" :key="index" :type="btnItem.style"
+                 :size="btnItem.size" :plain="btnItem.plain" :round="btnItem.round" :disabled="btnItem.disabled"
+                 @click="btnItem.event()">
+        {{btnItem.name}}
+      </component>
+    </div>
+
   </div>
 </template>
 <script>
@@ -17,12 +35,14 @@
     },
     props: {
       autoFormID: {type: String},//自动表单唯一ID值（根据业务自定义）
+      type: {type: String},//自动表单唯一ID值（根据业务自定义）
       /*
       * auto-form Button information
       * type:  'submit','reset','custom'
       * style: primary,success,warning,danger,info,text
       */
       buttonInfo: {type: Array},
+      roleButtonList: {type: Array},//按钮权限信息对照列表
     },
     created() {
 
