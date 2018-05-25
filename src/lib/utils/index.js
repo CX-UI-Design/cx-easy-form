e1990344ec1b9c576d90d2416d69318aa4d7ce3b
BroccoliSpring <Lensgcx@163.com>
2018-05-25 09:31:24 +0800

    utils methods
    utils methods

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -25,6 +25,43 @@ export const dynamicLoading = {
   }
 }
 
+/*
+ * addEventListener（绑定Dom元素的监听事件）
+ *
+ * target：监听对象
+ * type：监听函数类型，如click,mouseover
+ * func：监听函数
+*/
+export function addEventHandler(target, type, func) {
+  if (target.addEventListener) {
+    //监听IE9，谷歌和火狐
+    target.addEventListener(type, func, false);
+  } else if (target.attachEvent) {
+    target.attachEvent("on" + type, func);
+  } else {
+    target["on" + type] = func;
+  }
+}
+
+/*
+ * :
+ *removeEventHandler （移除Dom元素的监听事件）
+ * target：监听对象
+ * type：监听函数类型，如click,mouseover
+ * func：监听函数
+ */
+export function removeEventHandler(target, type, func) {
+  if (target.removeEventListener) {
+    //监听IE9，谷歌和火狐
+    target.removeEventListener(type, func, false);
+  } else if (target.detachEvent) {
+    target.detachEvent("on" + type, func);
+  } else {
+    delete target["on" + type];
+  }
+}
+
+
 //Crypto function
 export const crypto = {
   /**
